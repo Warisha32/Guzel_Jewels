b3f2a3ecd3aefe2ff2fbe41a85fbead83175e3a9
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2023-06-21 13:41:24 +0530

    cc

---
+++ guzels/src/Pages/Product.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const products = [
+  { id: 1, name: 'Product 1', price: 10 },
+  { id: 2, name: 'Product 2', price: 20 },
+  { id: 3, name: 'Product 3', price: 30 },
+];
+
+function ProductPage() {
+  return (
+    <div>
+      <h2>Featured Products</h2>
+      <div className="product-list">
+        {products.map((product) => (
+          <div key={product.id} className="product-item">
+            <h3>{product.name}</h3>
+            <p>Price: ${product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
